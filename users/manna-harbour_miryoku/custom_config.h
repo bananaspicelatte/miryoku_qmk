f9160f007953efad6fc3093988cde23d710173f2
bananaspicelatte <125724465+bananaspicelatte@users.noreply.github.com>
2023-06-14 17:48:32 +0900

    Update custom_config.h

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -15,7 +15,7 @@
 ) \
 LAYOUT_split_3x6_3( \
 XXX,  K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09,  XXX, \
-LGUI_T(KC_CAPS),  K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,  KC_RGUI), \
+LGUI_T(KC_CAPS),  K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,  KC_RGUI, \
 LGUI(KC_SPACE),  K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29,  XXX , \
                   K32,  K33,  K34,         K35,  K36,  K37 \
 )
